e507eac5465a889606400e7ed9b73606dd216a65
mohsin <mohsin.techuick@gmail.com>
2020-10-21 15:01:17 +0500

    updated database

--- techuick_blog.sql
+++ techuick_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 16, 2020 at 10:34 AM
+-- Generation Time: Oct 21, 2020 at 10:00 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -40,14 +40,14 @@ CREATE TABLE IF NOT EXISTS `blogs` (
   `status` tinyint(4) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `blogs`
 --
 
 INSERT INTO `blogs` (`id`, `user_id`, `title`, `description`, `thumbnail`, `created_at`, `updated_at`, `status`) VALUES
-(24, 14, 'Hello wprld!', 'hdfvfff  f', NULL, '2020-10-16 15:30:27', NULL, 0);
+(31, 26, 'how to earn from youtube?', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat voluptatem neque, possimus inventore nostrum amet, tempora magni id quod, itaque fugit incidunt laborum dolore nemo nam aliquid tempore accusantium laboriosam.\r\n	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat voluptatem neque, possimus inventore nostrum amet, tempora magni id quod, itaque fugit incidunt laborum dolore nemo nam aliquid tempore accusantium laboriosam.', 'db_images/1603098858home-bg.jpg', '2020-10-19 14:14:18', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -65,7 +65,16 @@ CREATE TABLE IF NOT EXISTS `comments` (
   PRIMARY KEY (`id`),
   KEY `comments_ibfk_1` (`blog_id`),
   KEY `comments_ibfk_2` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `comment`, `blog_id`, `user_id`, `posted_date`) VALUES
+(52, 'this post is very  useful', 31, 26, '2020-10-19 09:16:23'),
+(53, 'pakistan post', 31, 26, '2020-10-19 09:16:55'),
+(54, 'my comment', 31, 27, '2020-10-19 09:30:57');
 
 -- --------------------------------------------------------
 
@@ -92,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `roles` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `role` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=313 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=314 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `roles`
@@ -123,15 +132,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   KEY `role_id` (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `phone`, `profile_pic`, `password`, `created_at`, `updated_at`, `role_id`) VALUES
-(13, 'mohsin', 'mustafa', 'mohsinmustafa576@gmail.com', '03106266871', 'db_images/1602663219cup.png', '5f4dcc3b5aa765d61d8327deb882cf99', '2020-10-14 13:13:39', NULL, 2),
-(14, 'ali', 'akbar', 'aliahmad@gmail.com', '03457119986', 'db_images/1602761201eagle.png', '5f4dcc3b5aa765d61d8327deb882cf99', '2020-10-14 13:13:39', NULL, 1);
+(24, 'admin', 'admin', 'admin@gmail.com', '03106266871', 'db_images/1603098736cup.png', '21232f297a57a5a743894a0e4a801fc3', '2020-10-19 14:12:16', NULL, 2),
+(26, 'mohsin', 'mustafa', 'user@gmail.com', '03457119986', 'db_images/1603098797eagle.png', 'ee11cbb19052e40b07aac0ca060c23ee', '2020-10-19 14:13:17', NULL, 1),
+(27, 'ali', 'raza', 'user2@gmail.com', '02147777', 'db_images/1603099835home-bg.jpg', 'ee11cbb19052e40b07aac0ca060c23ee', '2020-10-19 14:30:35', NULL, 1);
 
 --
 -- Constraints for dumped tables
